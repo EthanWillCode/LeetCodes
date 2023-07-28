50e8c4b1a81f4ee23393a7b4c86d0bbdc1cab9e1
EthanWillCode <Aethil@yahoo.com>
2023-07-27 22:28:16 -0500

    Completed this challenge using if else spam.

--- 12. Integer to Roman/intToRoman.js
+++ 12. Integer to Roman/intToRoman.js
@@ -15,10 +15,47 @@ let test3 = 1994;
 var intToRoman = function(num) {
     let i = num;
     let output = [];
-    while(i!==0){
-        if(){};
-        if(){};
-        if(){};
+    while(i > 0){
+        if(i >= 1000){
+            output.push('M')
+            i -= 1000
+        }else if(i>=900){
+            output.push('CM')
+            i -= 900
+        }else if(i>=500){
+            output.push('D')
+            i -= 500
+        }else if(i>=400){
+            output.push('CD')
+            i -= 400
+        }else if(i>=100){
+            output.push('C')
+            i -= 100
+        }else if(i>=90){
+            output.push('XC')
+            i -= 90
+        }else if(i>=50){
+            output.push('L')
+            i -= 50
+        }else if(i>=40){
+            output.push('XL')
+            i -= 40
+        }else if(i>=10){
+            output.push('X')
+            i -= 10
+        }else if(i>=9){
+            output.push('IX')
+            i -= 9
+        }else if(i>=5){
+            output.push('V')
+            i -= 5
+        }else if(i>=4){
+            output.push('IV')
+            i -= 4
+        }else{
+            output.push('I')
+            i -= 1
+        }
     }
     return output.join("");
 };
@@ -27,3 +64,7 @@ var intToRoman = function(num) {
 // test1 should yield "III";
 // test2 should yield "LVIII";
 // test3 should yield "MCMXCIV";
+
+console.log(intToRoman(test1));
+console.log(intToRoman(test2));
+console.log(intToRoman(test3));
