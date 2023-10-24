57bcf555f2da1b039e8a9ac9c1c38c3f6ef2e10b
EthanWillCode <Aethil@yahoo.com>
2023-10-23 21:27:39 -0500

    A rather brute force method, but it worked. That makes for me passing my second medium!

--- 3. Longest Substring Without Repeating Charactors/lengthOfLongestSubstring.js
+++ 3. Longest Substring Without Repeating Charactors/lengthOfLongestSubstring.js
@@ -1,7 +1,37 @@
-let test1 = "abcabcbb";
-let test2 = "bbbbb";
-let test3 = "pwwkew";
+let s1 = "abcabcbb";
+let s2 = "bbbbb";
+let s3 = "pwwkew";
+let s4 = "abcdefgabc"
 
 var lengthOfLongestSubstring = function(s) {
-    
+    let lengthOfLongestSubstringSoFar = 0;
+
+    for(i=0;i<s.length;i++){
+        let counter = 1;
+        let j = i+1;
+        let hold =[s[i]]
+        while(s[i]!=s[j] && j< s.length ){
+            hold.push(s[j])
+            counter++;
+            j++
+            if(hold.includes(s[j])){
+            
+                break;
+            }
+        }
+        if(counter > lengthOfLongestSubstringSoFar){
+            lengthOfLongestSubstringSoFar = counter;
+        }
+
+        if(lengthOfLongestSubstring > (s.length -i)) {
+            break;
+        }
+    }
+    return lengthOfLongestSubstringSoFar;
 };
+
+
+console.log(lengthOfLongestSubstring(s1));
+console.log(lengthOfLongestSubstring(s2));
+console.log(lengthOfLongestSubstring(s3));
+console.log(lengthOfLongestSubstring(s4));
